1f200c0b1746be4524e4f25415b588a622050835
vidya sagar <vdsagar3230@gmail.com>
2023-09-05 02:24:22 +0000

    commit

--- models/example/incr_data.sql
+++ models/example/incr_data.sql
@@ -1,9 +1,10 @@
-{{ config(materialized="incremental") }}
+{{ config(materialized="incremental",
+unique_ID="ID") }}
 with order_data as 
 (
     select * from {{ref('stg_orders')}} 
     {%if is_incremental()%}
-     where order_date>= (select max(ORDER_DATE) from {{this}})
+     where order_date>= (select max(order_date) from {{this}})
      {%endif%}
     )
 select * from  order_data
